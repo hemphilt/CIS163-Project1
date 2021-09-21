ab28042cd8eb6f6532f31791852775bc66381176
hemphilt <91099927+hemphilt@users.noreply.github.com>
2021-09-21 17:12:08 -0400

    Update CountDownTimerPanelSwing.java

--- CountDownTimerPanelSwing.java
+++ CountDownTimerPanelSwing.java
@@ -1,4 +1,4 @@
-package project1;
+package Project1;
 
 import javax.swing.*;
 
@@ -15,75 +15,115 @@ import java.awt.event.ActionListener;
 public class CountDownTimerPanelSwing extends JPanel {
 
 	private CountDownTimer watch;
-	private Timer javaTimer;
+    private Timer javaTimer;
 
-	private JButton startButton, stopButton, saveButton, loadButton, addButton, stringInputButton, continueButton;;
-	private JTextField hourField, minField, secondField, addSecondsField, newStringField;
+    private JButton startButton, stopButton, saveButton, loadButton, addButton, stringInputButton, continueButton;
+    private JTextField hourField, minField, secondField, addSecondsField, newStringField;
 
-	private JLabel lblTime;
+    private JLabel lblTime;
 
-	public CountDownTimerPanelSwing() {
+    public CountDownTimerPanelSwing() {
 
-		// create the game object as well as the GUI1024 Frame
-		watch = new CountDownTimer();
-		javaTimer = new Timer(1000, new TimerListener());
+        // create the game object as well as the GUI1024 Frame
+        watch = new CountDownTimer();
+        javaTimer = new Timer(1000, new TimerListener());
 
-		setLayout(new GridLayout(9, 2));
-		setBackground(Color.lightGray);
+        setLayout(new GridLayout(9, 4));
+        setBackground(Color.lightGray);
+        
+        //column1
+        hourField = new JTextField();
+        add(hourField);
+        add(new JLabel("Hours:"));
+        
+        minField = new JTextField();
+        add( minField );
+        add(new JLabel("Minutes:"));
+        
+        secondField = new JTextField();
+        add(secondField);
+        add(new JLabel("Seconds:"));
 
-		// Code goes on...
-		startButton = new JButton("Start");
-		add(startButton);
+        stopButton = new JButton("Stop");
+        add(stopButton);
+        
+        startButton = new JButton("Start");
+        add(startButton);
+        
+        loadButton = new JButton("Load");
+        add(loadButton);
+        
+        saveButton = new JButton("Save");
+        add(saveButton);
+        
+        addButton = new JButton("Add");
+        add(addButton);
+        
+        addSecondsField = new JTextField();
+        add(addSecondsField);
+        
+        stringInputButton = new JButton("New");
+        add(stringInputButton);
+        
+        newStringField = new JTextField();
+        add(newStringField);
+        
+        continueButton = new JButton("Continue");
+        add(continueButton);
 
-		lblTime = new JLabel();
-		lblTime.setText(watch.toString());
-		add(lblTime);
+        lblTime = new JLabel();
+        lblTime.setText(watch.toString());
+        add(lblTime);
 
-		add(new JLabel("Time:"));
+        add(new JLabel("0:00:00"));
+        
+        add(new JLabel("Time:"));
+        
+        //column 2
+        
+        //column3
+        startButton.addActionListener(new ButtonListener());
 
-		startButton.addActionListener(new ButtonListener());
-		// Code goes on...
+    }
 
-	}
+    private class ButtonListener implements ActionListener {
 
-	private class ButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent event) {
 
-		public void actionPerformed(ActionEvent event) {
+            if (event.getSource() == stopButton) {
+                javaTimer.stop();
+            }
 
-			if (event.getSource() == stopButton) {
-				javaTimer.stop();
+            if (event.getSource() == startButton) {
+            	int mins, sec, milli;
+                try {
+                    mins = Integer.parseInt(hourField.getText());
+                    sec = Integer.parseInt(minField.getText());
+                    milli = Integer.parseInt(secondField.getText());
+                    watch = new CountDownTimer(mins, sec, milli);
+                    javaTimer.start();
+                } catch (NumberFormatException io) {
+                    JOptionPane.showMessageDialog(null, "Enter an integer in all fields");
+                } catch (IllegalArgumentException e) {
+                    JOptionPane.showMessageDialog(null, "Error in field");
+                }
+            }
+
+            lblTime.setText(watch.toString());
+        }
+
+    }
+
+    private class TimerListener implements ActionListener {
+
+        public void actionPerformed(ActionEvent e) {
+            try {
+                // watch.sub(1);
+                lblTime.setText(watch.toString());
+            }
+            catch (Exception exception) {
+
 			}
-
-			if (event.getSource() == startButton) {
-				int mins, sec, milli;
-				try {
-					mins = Integer.parseInt(hourField.getText());
-					sec = Integer.parseInt(minField.getText());
-					milli = Integer.parseInt(secondField.getText());
-					watch = new CountDownTimer(mins, sec, milli);
-					javaTimer.start();
-				} catch (NumberFormatException io) {
-					JOptionPane.showMessageDialog(null, "Enter an integer in all fields");
-				} catch (IllegalArgumentException e) {
-					JOptionPane.showMessageDialog(null, "Error in field");
-				}
-			}
-
-			lblTime.setText(watch.toString());
-		}
-
-	}
-
-	private class TimerListener implements ActionListener {
-
-		public void actionPerformed(ActionEvent e) {
-			try {
-				// watch.sub(1);
-				lblTime.setText(watch.toString());
-			}
-			catch (Exception exception) {
-
-			}
-		}
-	}
+        }
+    }
 }
