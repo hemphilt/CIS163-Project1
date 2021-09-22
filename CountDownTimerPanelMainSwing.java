0a57cff03b9aec9d2bb419064fe110c897a11ff9
Getty254 <53070873+Getty254@users.noreply.github.com>
2021-09-22 07:58:08 -0400

    Update CountDownTimerPanelMainSwing.java

--- CountDownTimerPanelMainSwing.java
+++ CountDownTimerPanelMainSwing.java
@@ -1,47 +1,46 @@
-package project1;
+package Project1;
 
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
 public class CountDownTimerPanelMainSwing extends JPanel {
 
 	private JMenuItem quitItem;
 	private JMenuItem suspendItem;
-	private JButton supsendTrue;
+	private JButton suspendTrue;
 	private JButton suspendFalse;
-
+	
 	public CountDownTimerPanelMainSwing(JMenuItem quitItem, JMenuItem suspendItem) {
 		this.quitItem = quitItem;
 		this.suspendItem = suspendItem;
-
+		
 		JPanel panel = new JPanel();
+		panel.add(new CountDownTimerPanelSwing());
+		panel.add(new CountDownTimerPanelSwing());
 		panel.add(new CountDownTimerPanelSwing());
 
 		suspendFalse = new JButton("Suspend false");
-		supsendTrue = new JButton("Suspend true");
+		suspendTrue = new JButton("Suspend true");
 		add(panel);
 		add (suspendFalse);
-		add (supsendTrue);
-
+		add (suspendTrue);
+	
 		quitItem.addActionListener(new Mylistener());
 		suspendItem.addActionListener(new Mylistener());
-		supsendTrue.addActionListener(new Mylistener());
+		suspendTrue.addActionListener(new Mylistener());
 		suspendFalse.addActionListener(new Mylistener());
 	}
-
+	
 	private class Mylistener implements ActionListener {
 		public void actionPerformed(ActionEvent e){
 			if(e.getSource() == quitItem){
 				System.exit(1);
 			}
-
-			//			if (e.getSource() == suspendFalse)
-			//				CountDownTimer.setSuspend(false);
-			//			if (e.getSource() == supsendTrue)
-			//				CountDownTimer.setSuspend(true);
-		}
-
+			if (e.getSource() == suspendFalse)
+				CountDownTimer.setSuspend(false);
+			if (e.getSource() == suspendTrue)
+				CountDownTimer.setSuspend(true);
+		}		
 	}
 }
